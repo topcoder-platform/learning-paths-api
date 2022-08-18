df6a41a1c31e750062e8a36014180276abd7c09a
Brooke <brooke.souza@topcoder.com>
2022-08-18 10:25:57 -0700

    TCA-316 #comment This commit switches the IAC config from json to yaml and fixes a bug in the create-stack script #time 15m

--- src/utils/certificate-image-generator/create-stack.sh
+++ src/utils/certificate-image-generator/create-stack.sh
@@ -2,31 +2,31 @@
 
 # get the env
 env=$1
-echo $env
 if [ -z $env ]
   then
     echo "Enter env name:"
     read ENV
     env=$ENV
 fi
-echo "Environment: $env"
+envMessage="Environment:"
 
 # get the stack and queue names
 stackName="TCA-Certificate-Generator"
 queueName="tca-certficate-generator"
 if [ -z $env ]
   then
-    echo "No env $env"
+    echo "$envMessage No Env"
   else
+    echo "$envMessage $env"
     stackName="$stackName-$env"
-    queueName="$queuName-$env"
+    queueName="$queueName-$env"
 fi
 echo "Stack name: $stackName"
 echo "Queue name: $queueName"
 
 # read in the template body
 # NOTE: this is the path based on running the script from the root dir
-templateBody=$(<./src/utils/certificate-image-generator/certificate-image-generator.iac.json)
+templateBody=$(<./src/utils/certificate-image-generator/certificate-image-generator.yml)
 
 # create the stack w/the params
 aws cloudformation create-stack --stack-name $stackName --template-body "$templateBody" --parameters ParameterKey=TCAGenerateCertificateQueueName,ParameterValue=$queueName

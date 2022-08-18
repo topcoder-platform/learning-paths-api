916a31b82b5270a2fad2879eee8f5ba40fb4f3b4
Brooke <brooke.souza@topcoder.com>
2022-08-18 16:12:54 -0700

    TCA-317 clean-up #time 5m

--- src/common/queue-helper.js
+++ src/common/queue-helper.js
@@ -8,22 +8,22 @@ AWS.config.update({
 // Create an SQS service object
 const sqs = new AWS.SQS();
 
-async function sendMessageAsync(queue, messageBody, messageTitle, messageAuthor) {
+async function sendMessageAsync(queueName, body, title, author) {
 
     const params = {
         DelaySeconds: 10,
         MessageAttributes: {
             'Title': {
                 DataType: 'String',
-                StringValue: messageTitle
+                StringValue: title
             },
             'Author': {
                 DataType: 'String',
-                StringValue: messageAuthor
+                StringValue: author
             },
         },
-        MessageBody: JSON.stringify(messageBody),
-        QueueUrl: `${process.env.QUEUE_URL}${queue}`,
+        MessageBody: JSON.stringify(body),
+        QueueUrl: `${process.env.QUEUE_URL}${queueName}`,
     };
 
     return sqs.sendMessage(params)

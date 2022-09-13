d43f6f9ab404970249b774b0c84120e7fcdcea6d
Chris McCann <chris.mccann@topcoder.com>
2022-09-13 13:38:34 -0700

    Modifies serverless deploy scripts to source env vars

--- src/utils/mongodb-trigger-handler/deploy_dev.sh
+++ src/utils/mongodb-trigger-handler/deploy_dev.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
+source ./.dev.env
 sls deploy --aws-profile 811668436784_AWSPowerUserPlusIAM  --stage dev
--- src/utils/mongodb-trigger-handler/deploy_prod.sh
+++ src/utils/mongodb-trigger-handler/deploy_prod.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
+source ./.prod.env
 sls deploy --aws-profile 409275337247_AWSPowerUserPlusIAM --stage prod

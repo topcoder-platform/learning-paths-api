40f93d6562d31505f67bddc490af9878f778bec7
Brooke <brooke.souza@topcoder.com>
2022-09-14 16:51:17 -0700

    TCA-439 #comment This commit improves the README; adds a test event for the lambda; improves error handling in the lambda #time 2h

--- src/utils/certificate-image-generator/README.md
+++ src/utils/certificate-image-generator/README.md
@@ -23,13 +23,9 @@ The yaml located at `./certificate-image-generator.yml` is the CloudFormation re
 
 There is a helper bash script at `./deploy-stack.sh` that will generate the `aws cloudformation` command for the stage specified.
 
-The script supports a stage argument that will be added as a suffix to the Stack and dependent service names.
-
-If no stage is provided, the default names will be used.
+The script requires a stage argument that will be added as a suffix to the Stack and dependent service names.
 
 There is also a helper package script called `cert-gen:deploy-stack` to make it even easier.
-
-<b>with stage</b>
 
 ```
 % npm run cert-gen:deploy-stack myStage
@@ -44,58 +40,40 @@ Bucket name: tca-certificate-generator-s3-myStage
 CDN domain: tca-certificate-generator-s3-myStage.s3.amazonaws.com
 ```
 
-<b>without stage</b>
-
-```
-% npm run cert-gen:deploy-stack
-
-> topcoder-learning-paths-api@1.0.0 cert-gen:deploy-stack
-> sh src/utils/certificate-image-generator/deploy-stack.sh
-
-Stage:
-Stack name: TCA-Certificate-Generator
-Queue name: tca-certificate-generator-sqs
-Bucket name: tca-certificate-generator-s3
-CDN domain: tca-certificate-generator-s3.s3.amazonaws.com
-Alias: tca.topcoder-dev.com
-```
-
 <b>There are also a couple checks in the script that you can silence by adding a 2nd argument of `Y`.<b>
 
 ```
 % npm run cert-gen:deploy-stack myStage Y
 ```
-<b>or without an stage specified:<b>
-```
-% npm run cert-gen:deploy-stack "" Y
-```
 
 ### Deploying the Queue with local envioronment variables
 
-There are some environment variables that must be set in order to create the stack:
+Here are the environment variables that must be set in order to create the stack:
 
 ```
 # The domain at which the cert images will be hosted. The deploy-stack script will
 # create a Route 53 record for this cert
-CERT_IMAGE_DOMAIN=topcoder-dev.com 
-
-# The cert for the domain defined in the alias. This can be found in the Route 53
-# record for the selected wildcard domain (e.g. *.topcoder-dev.com).
-CERT_IMAGE_CERT_ARN=arn:aws:acm:us-east-1:811668436784:certificate/8e56f24a-1eaf-4289-8c21-db2c0761ecf4
-
-# The hosted zone ID for the CERT_IMAGE_DOMAIN domain. This is environment-dependent.
-CERT_IMAGE_HOSTED_ZONE_ID=Z2CIRG3R0ZSGFQ
-```
-
-## Configuring Queue
-
-In order to send messages to the queue, you'll need to add the following environment variables:
+CERT_IMAGE_DOMAIN=topcoder-dev.com
 
 ```
+### Deploy Changes to Generator
+
+Changes to the serverless functions required to create and serve images are automatically
+deployed every time the deploy-stack script is run, even if there are no other changes.
+
+## Configuring API
+
+In order to send data to the queue, you'll need to add the following environment variables:
+
+```
+# The bucket in which cert images should be stored
+CERT_BUCKET=tca-certificate-generator-s3-dev
+
+# The subdomain of the alias for the CDN in which the images are stored.
+# This will combine w/the CERT_IMAGE_DOMAIN to create the FQDN for the image.
+# This should follow the pattern of `tca-myStage` for all non-production stages.
+CERT_IMAGE_SUBDOMAIN=tca-dev
+
 # The URL for the queue that was created from the deploy-stack script.
 CERT_IMAGE_QUEUE=https://sqs.us-east-1.amazonaws.com/811668436784/tca-certificate-generator-sqs
 ```
-
-## Deploy Changes to Generator
-
-TODO
--- src/utils/certificate-image-generator/generate-certificate-image/index.js
+++ src/utils/certificate-image-generator/generate-certificate-image/index.js
@@ -1,20 +1,21 @@
-const chromium = require('@sparticuz/chrome-aws-lambda');
-const AWS = require('aws-sdk');
+const chromium = require('@sparticuz/chrome-aws-lambda')
+const AWS = require('aws-sdk')
 
 exports.handler = async (event) => {
 
     if (!event?.Records?.[0].body) {
         const errorMessage = `The event must have a body.`
         console.error(errorMessage, event)
-        throw new Error(errorMessage);
+        throw new Error(errorMessage)
     }
 
-    let browser = undefined;
+    let browser = undefined
 
     try {
 
-        const params = JSON.parse(event.Records[0].body);
-        validateParams(params);
+        const body = event.Records[0].body
+        const params = body.constructor.name === 'String' ? JSON.parse(body) : body
+        validateParams(params)
 
         // set up the chromium headless browser
         browser = await chromium.puppeteer.launch({
@@ -23,54 +24,56 @@ exports.handler = async (event) => {
             executablePath: await chromium.executablePath,
             headless: chromium.headless,
             ignoreHTTPSErrors: true,
-        });
+        })
 
         // go to the page
-        const page = await browser.newPage();
-        await page.goto(params.url);
+        const page = await browser.newPage()
+        await page.goto(params.url)
 
         // get the screenshot of the page
-        let screenshot = undefined;
-        const imageConfig = { type: 'jpeg' };
+        let screenshot = undefined
+        const imageConfig = { type: 'jpeg' }
 
         // if there's an element, wait for it and save it
         if (!!params.screenshotSelector) {
 
             // wait for the specific element to appear
-            await page.waitForSelector(params.screenshotSelector);
+            await page.waitForSelector(params.screenshotSelector, { timeout: 1000 })
 
             // select the element
-            const element = await page.$(params.screenshotSelector);
+            const element = await page.$(params.screenshotSelector)
 
             // take a screenshot of the element
-            screenshot = await element.screenshot(imageConfig);
+            screenshot = await element.screenshot(imageConfig)
 
         } else {
 
             // take a screenshot of the entire page
-            screenshot = await page.screenshot(imageConfig);
+            screenshot = await page.screenshot(imageConfig)
         }
 
         await putObjectToS3Async(
             params.bucket,
             params.filePath,
             screenshot
-        );
+        )
 
-        console.info(`Created ${params.filePath} in ${params.bucket}`)
+        console.info(`Created cert image for ${params.url}`)
+        console.info(`Saved cert image at ${params.filePath} in ${params.bucket}`)
 
     } catch (error) {
-        // TODO: error handling
-        console.error(error, event);
+        console.error(error)
+        console.error(event?.Records?.[0]?.body)
+        throw error
 
     } finally {
         // if a browser was created,
         // make sure to close it
         if (!!browser?.close) {
-            await browser.close();
+            await browser.close()
         }
     }
-};
+}
 
 async function putObjectToS3Async(bucket, key, image) {
 
@@ -80,7 +83,8 @@ async function putObjectToS3Async(bucket, key, image) {
         Body: image
     }
 
-    await new AWS.S3().putObject(params).promise()
+    await new AWS.S3().putObject(params)
+        .promise()
 }
 
 function validateParams(params) {
@@ -90,11 +94,11 @@ function validateParams(params) {
         'filePath',
         'url',
     ]
-        .find(param => !params?.[param]);
+        .find(param => !params?.[param])
 
     if (requiredParam) {
         const errorMessage = `The ${requiredParam} param is required.`
         console.error(errorMessage, params)
-        throw new Error(errorMessage);
+        throw new Error(errorMessage)
     }
 }

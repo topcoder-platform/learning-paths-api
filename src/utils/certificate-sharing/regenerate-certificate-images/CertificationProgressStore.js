49bd901b0d6275a60e274437ba364791a017dd16
Brooke <brooke.souza@topcoder.com>
2022-10-06 11:36:20 -0700

    TCA-498

--- src/utils/certificate-sharing/regenerate-certificate-images/CertificationProgressStore.js
+++ src/utils/certificate-sharing/regenerate-certificate-images/CertificationProgressStore.js
@@ -1,18 +1,18 @@
 const dbHelper = require('../../../common/helper')
 
 /**
- * Gets all the Certificate Progress records that are completed
+ * Gets all the Certification Progress records that are completed
  */
 async function getAllCompleted() {
 
     const certificationProgresses = await dbHelper.scanAll('CertificationProgress')
 
-    console.log(`Found ${certificationProgresses.length} progress records.`)
+    console.log(`Found ${certificationProgresses.length} certificataoin progress records.`)
 
     const completedCertifications = certificationProgresses
         .filter(certProgress => certProgress.status === 'completed')
 
-    console.log(`Found ${completedCertifications.length} completed progress records.`)
+    console.log(`Found ${completedCertifications.length} completed certification progress records.`)
 
     return completedCertifications
 }

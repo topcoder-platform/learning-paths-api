17f6c26f77cc0372e43d06ad3e4add9710e8d482
Kiril Kartunov <kiril.kartunov@gmail.com>
2022-12-16 12:08:12 +0200

    remove console debug

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -23,7 +23,6 @@ fs
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    console.log(file, path.join(__dirname, file), model)
     db[model.name] = model;
   });
 

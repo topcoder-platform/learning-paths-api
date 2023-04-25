51319f3a239eac6f8fac4227bf4f45744444c4f9
Kiril Kartunov <kiril.kartunov@gmail.com>
2023-04-25 10:33:06 +0300

    TCA-1205 EMSI data seeders

--- src/db/seeders/after_data_loaded/20230406213337-add-emsi-skills-to-certifications.js
+++ src/db/seeders/after_data_loaded/20230406213337-add-emsi-skills-to-certifications.js
@@ -3,28 +3,41 @@
 /** @type {import('sequelize-cli').Migration} */
 
 const db = require('../../models');
-const EMSIdata = require('./tca-certs-emsi-data');
+const CertsEMSIdata = require('./tca-certs-emsi-data');
+const CoursesEMSIdata = require('./tca-courses-emsi-data');
 
 module.exports = {
   async up(queryInterface, Sequelize) {
+    // CERTS
     // TCA Web Dev cert
     const tcWebDev = await db.TopcoderCertification.findOne({
       where: { dashedName: 'web-development-fundamentals' }
     });
 
     await tcWebDev.update({
-      emsiSkills: EMSIdata.tcWebDev
+      emsiSkills: CertsEMSIdata.tcWebDev
     });
 
 
     // TCA DS cert
-    const tcDataSci = await await db.TopcoderCertification.findOne({
+    const tcDataSci = await db.TopcoderCertification.findOne({
       where: { dashedName: 'data-science-fundamentals' }
     });
 
     await tcDataSci.update({
-      emsiSkills: EMSIdata.tcDataSci
+      emsiSkills: CertsEMSIdata.tcDataSci
     });
+
+    // COURSES
+    const courses = await db.FccCourse.findAll();
+    for (const course of courses) {
+      // if we have data - update
+      if (CoursesEMSIdata[course.key]) {
+        await course.update({
+          emsiSkills: CoursesEMSIdata[course.key]
+        });
+      }
+    }
   },
 
   async down(queryInterface, Sequelize) {
@@ -39,12 +52,20 @@ module.exports = {
 
 
     // TCA DS cert
-    const tcDataSci = await await db.TopcoderCertification.findOne({
+    const tcDataSci = await db.TopcoderCertification.findOne({
       where: { dashedName: 'data-science-fundamentals' }
     });
 
     await tcDataSci.update({
       emsiSkills: null
     });
+
+    // COURSES
+    const courses = await db.FccCourse.findAll();
+    for (const course of courses) {
+      await course.update({
+        emsiSkills: null
+      });
+    }
   }
 };
--- src/db/seeders/after_data_loaded/tca-courses-emsi-data.js
+++ src/db/seeders/after_data_loaded/tca-courses-emsi-data.js
@@ -4,161 +4,7 @@
 // Exact data is TBD in https://topcoder.atlassian.net/browse/TCA-1196
 
 module.exports = {
-    tcWebDev: [
-        {
-            name: "Cascading Style Sheets (CSS)",
-            emsiId: "KS121F45VPV8C9W3QFYH",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Markup Languages",
-            emsiId: "KS1233M6Q271L5ZNBP86",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "React.js",
-            emsiId: "KSDJCA4E89LB98JAZ7LZ",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "React Redux",
-            emsiId: "KSQOOX1S2DYD0E1VVZ5X",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Style Sheets",
-            emsiId: "KS7G6WX6133QYMD9XSY8",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Mixins",
-            emsiId: "KSP39K990D2KPFTICJDQ",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Responsive Web Design",
-            emsiId: "KS4401Q6TM90DBQSF645",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "HyperText Markup Language (HTML)",
-            emsiId: "KS1200578T5QCYT0Z98G",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "JQuery",
-            emsiId: "KS125KQ5WKM97L66SSQY",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "JavaScript (Programming Language)",
-            emsiId: "KS1200771D9CR9LB4MWW",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "HTML Components",
-            emsiId: "KSD9Z9YAZHT0I55COV7R",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "CSS Animations",
-            emsiId: "KS122KM662BPX3KW64L9",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Flexbox",
-            emsiId: "KSG8V19PB8K7PM95X4HW",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "CSS Grid",
-            emsiId: "BGSA9EF681216841CAE2",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Object-Oriented Programming (OOP)",
-            emsiId: "KS1275Z6908VHW76FRXH",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "ECMAScript (C Programming Language Family)",
-            emsiId: "KS1239R64LDPZDFS25RV",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "ECMAScript 2015",
-            emsiId: "KSUK6OFU4EA534NO9T4D",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Debugging",
-            emsiId: "KS122SG5Y0Q5YQM7V8KT",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Regular Expressions",
-            emsiId: "KS128GV6Y1LLQBBCNMFH",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Scripting",
-            emsiId: "KS7G0C36YW29VS8KPC05",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Data Structures",
-            emsiId: "KS122Q26BWQ1PK3Y74C0",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Programming Concepts",
-            emsiId: "KS1284363T46K06LX5XB",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
+    "back-end-development-and-apis": [
         {
             name: "NoSQL",
             emsiId: "KS1272Y6RFNZKNGWDWMB",
@@ -228,9 +74,74 @@ module.exports = {
             confidence: 1,
             learned: true,
             assessed: false
+        },
+        {
+            name: "JavaScript (Programming Language)",
+            emsiId: "KS1200771D9CR9LB4MWW",
+            confidence: 1,
+            learned: true,
+            assessed: false
         }
     ],
-    tcDataSci: [
+    "scientific-computing-with-python": [
+        {
+            name: "Relational Databases",
+            emsiId: "KS122QK6TH01FWQ9X8CH",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Python (Programming Language)",
+            emsiId: "KS125LS6N7WP4S6SFTCK",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Machine Learning",
+            emsiId: "KS1261Z68KSKR1X31KS3",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Scientific Computing",
+            emsiId: "KS440BS6DPR9JGN0FDHG",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Scripting",
+            emsiId: "KS7G0C36YW29VS8KPC05",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Data Structures",
+            emsiId: "KS122Q26BWQ1PK3Y74C0",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Database Design",
+            emsiId: "KS122QL6NN356RRJT6RV",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Data Visualization",
+            emsiId: "KS122Q960QYMW3YH0YWF",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        }
+    ],
+    "data-visualization": [
         {
             name: "Application Programming Interface (API)",
             emsiId: "KS1208P6ZMZ4N872Y7X5",
@@ -314,10 +225,12 @@ module.exports = {
             confidence: 1,
             learned: true,
             assessed: false
-        },
+        }
+    ],
+    "machine-learning-with-python": [
         {
-            name: "Relational Databases",
-            emsiId: "KS122QK6TH01FWQ9X8CH",
+            name: "Mathematics",
+            emsiId: "KS126706DPFD3354M7YK",
             confidence: 1,
             learned: true,
             assessed: false
@@ -330,6 +243,13 @@ module.exports = {
             assessed: false
         },
         {
+            name: "Reinforcement Learning",
+            emsiId: "KS4LL1EBNO7YQ6BSVNIW",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
             name: "Machine Learning",
             emsiId: "KS1261Z68KSKR1X31KS3",
             confidence: 1,
@@ -337,8 +257,152 @@ module.exports = {
             assessed: false
         },
         {
-            name: "Scientific Computing",
-            emsiId: "KS440BS6DPR9JGN0FDHG",
+            name: "Natural Language Processing",
+            emsiId: "KS126VY6DLPDWFZD5VB8",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Convolutional Neural Networks",
+            emsiId: "ES3E181DDDBC374119D5",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Artificial Intelligence",
+            emsiId: "KS120BV6SR75RBKQH0G3",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "TensorFlow",
+            emsiId: "KS2GHRCYA6TRT29F1HOO",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Artificial Neural Networks",
+            emsiId: "KS120NN6SBW45D10MY36",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+    ],
+    "front-end-development-libraries": [
+        {
+            name: "Cascading Style Sheets (CSS)",
+            emsiId: "KS121F45VPV8C9W3QFYH",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Markup Languages",
+            emsiId: "KS1233M6Q271L5ZNBP86",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "React.js",
+            emsiId: "KSDJCA4E89LB98JAZ7LZ",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "React Redux",
+            emsiId: "KSQOOX1S2DYD0E1VVZ5X",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Style Sheets",
+            emsiId: "KS7G6WX6133QYMD9XSY8",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Mixins",
+            emsiId: "KSP39K990D2KPFTICJDQ",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Responsive Web Design",
+            emsiId: "KS4401Q6TM90DBQSF645",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "HyperText Markup Language (HTML)",
+            emsiId: "KS1200578T5QCYT0Z98G",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "JQuery",
+            emsiId: "KS125KQ5WKM97L66SSQY",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "JavaScript (Programming Language)",
+            emsiId: "KS1200771D9CR9LB4MWW",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+    ],
+    "javascript-algorithms-and-data-structures": [
+        {
+            name: "JavaScript (Programming Language)",
+            emsiId: "KS1200771D9CR9LB4MWW",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Object-Oriented Programming (OOP)",
+            emsiId: "KS1275Z6908VHW76FRXH",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "ECMAScript (C Programming Language Family)",
+            emsiId: "KS1239R64LDPZDFS25RV",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "ECMAScript 2015",
+            emsiId: "KSUK6OFU4EA534NO9T4D",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Debugging",
+            emsiId: "KS122SG5Y0Q5YQM7V8KT",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Regular Expressions",
+            emsiId: "KS128GV6Y1LLQBBCNMFH",
             confidence: 1,
             learned: true,
             assessed: false
@@ -358,12 +422,28 @@ module.exports = {
             assessed: false
         },
         {
-            name: "Database Design",
-            emsiId: "KS122QL6NN356RRJT6RV",
+            name: "Programming Concepts",
+            emsiId: "KS1284363T46K06LX5XB",
             confidence: 1,
             learned: true,
             assessed: false
         },
+        {
+            name: "HyperText Markup Language (HTML)",
+            emsiId: "KS1200578T5QCYT0Z98G",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Cascading Style Sheets (CSS)",
+            emsiId: "KS121F45VPV8C9W3QFYH",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        }
+    ],
+    "data-analysis-with-python": [
         {
             name: "Jupyter Notebook",
             emsiId: "KSXP0ABTAF9E0XOQ1MTF",
@@ -388,6 +468,13 @@ module.exports = {
         {
             name: "Seaborn",
             emsiId: "KSXGXI686SOYIFSM2YFY",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+        {
+            name: "Python (Programming Language)",
+            emsiId: "KS125LS6N7WP4S6SFTCK",
             confidence: 1,
             learned: true,
             assessed: false
@@ -427,51 +514,54 @@ module.exports = {
             learned: true,
             assessed: false
         },
+    ],
+    "2022/responsive-web-design": [
         {
-            name: "Mathematics",
-            emsiId: "KS126706DPFD3354M7YK",
+            name: "HTML Components",
+            emsiId: "KSD9Z9YAZHT0I55COV7R",
+            confidence: 1,
+            learned: true,
+            assessed: false
+        },
+
+        {
+            name: "CSS Animations",
+            emsiId: "KS122KM662BPX3KW64L9",
             confidence: 1,
             learned: true,
             assessed: false
         },
         {
-            name: "Reinforcement Learning",
-            emsiId: "KS4LL1EBNO7YQ6BSVNIW",
+            name: "Flexbox",
+            emsiId: "KSG8V19PB8K7PM95X4HW",
             confidence: 1,
             learned: true,
             assessed: false
         },
         {
-            name: "Natural Language Processing",
-            emsiId: "KS126VY6DLPDWFZD5VB8",
+            name: "CSS Grid",
+            emsiId: "BGSA9EF681216841CAE2",
             confidence: 1,
             learned: true,
             assessed: false
         },
         {
-            name: "Convolutional Neural Networks",
-            emsiId: "ES3E181DDDBC374119D5",
+            name: "Cascading Style Sheets (CSS)",
+            emsiId: "KS121F45VPV8C9W3QFYH",
             confidence: 1,
             learned: true,
             assessed: false
         },
         {
-            name: "Artificial Intelligence",
-            emsiId: "KS120BV6SR75RBKQH0G3",
+            name: "Responsive Web Design",
+            emsiId: "KS4401Q6TM90DBQSF645",
             confidence: 1,
             learned: true,
             assessed: false
         },
         {
-            name: "TensorFlow",
-            emsiId: "KS2GHRCYA6TRT29F1HOO",
-            confidence: 1,
-            learned: true,
-            assessed: false
-        },
-        {
-            name: "Artificial Neural Networks",
-            emsiId: "KS120NN6SBW45D10MY36",
+            name: "HyperText Markup Language (HTML)",
+            emsiId: "KS1200578T5QCYT0Z98G",
             confidence: 1,
             learned: true,
             assessed: false

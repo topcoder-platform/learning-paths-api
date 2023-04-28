25ad1f2db948beddfe0da0ccd60746736bffdfdd
Max Helali <max.helali@topcoder.com>
2023-04-28 16:35:18 -0700

    adding eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
+// Docs: https://eslint.org/docs/user-guide/configuring
 module.exports = {
-    parser: "@babel/eslint-parser",
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    es6: true,
+    ecmaVersion: 6,
+    sourceType: "module",
+    jsx: false,
+    requireConfigFile: false,
+  },
+  rules: {
+    "no-unused-vars": 2, // 0 = ignore, 1 = warn, 2= error
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
 };

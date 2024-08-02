f34ca90ccbbd5fdbc7735e28df6343be0809161d
Justin Gasper <jmgasper@gmail.com>
2024-08-02 15:51:08 +1000

    Update regeneration script to also do TCA certifications

--- src/utils/certificate-sharing/regenerate-certificate-images/CertificationProgressStore.js
+++ src/utils/certificate-sharing/regenerate-certificate-images/CertificationProgressStore.js
@@ -1,22 +1,33 @@
 const dbHelper = require('../../../common/dbHelper')
 
 /**
- * Gets all the Certification Progress records that are completed
+ * Gets all the FCC Certification Progress records that are completed
  */
 async function getAllCompleted() {
 
     const certificationProgresses = await dbHelper.findAll('FccCertificationProgress', ['resourceProvider'])
-
     console.log(`Found ${certificationProgresses.length} certification progress records.`)
-
     const completedCertifications = certificationProgresses
         .filter(certProgress => certProgress.status === 'completed')
-
     console.log(`Found ${completedCertifications.length} completed certification progress records.`)
 
     return completedCertifications
 }
 
+/**
+ * Gets all the TCA Certification Progress records that are completed
+ */
+async function getAllCompletedTCA() {
+
+    const certificationProgresses = await dbHelper.findAll('CertificationEnrollment')
+    console.log(`Found ${certificationProgresses.length} TCA certification progress records.`)
+    const completedCertifications = certificationProgresses
+        .filter(certProgress => certProgress.status === 'completed')
+    console.log(`Found ${completedCertifications.length} completed TCA certification progress records.`)
+
+    return completedCertifications
+}
 module.exports = {
     getAllCompleted,
+    getAllCompletedTCA
 }
